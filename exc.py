a0b07600b4f59a698107988f9621c71ad9f26ad6
alizaiter777 <alizaiter777@gmail.com>
2024-11-05 23:06:53 +0200

    create longest_subarray function

--- exc.py
+++ exc.py
@@ -1,5 +1,29 @@
 
 #using split function to split user input according to space
 #using map to convert each string to integer after splitting
-array = list(map(int, input("Enter list of integers (0s and 1s): ").split()))
+ARRAY = list(map(int, input("Enter list of integers (0s and 1s): ").split()))
 
+#create function to returns the lenght of the longest subarray
+def longest_subarray(ARRAY):
+    COUNT_ZERO = 0
+    COUNT_ONE = 0
+    LIST=[]
+    RESULT=0
+      #if integer is 0 then add it to count zero otherwise add it to ones
+    for num in ARRAY:
+        if num == 0:
+           COUNT_ZERO += 1
+           
+        else:
+            COUNT_ONE += 1
+           
+             # compare the counters and the smaller will be taken to returns the longest subarray
+        if COUNT_ZERO>COUNT_ONE:
+                LIST=COUNT_ONE
+        else:
+                LIST=COUNT_ZERO
+            
+            
+        RESULT = LIST*[1]+LIST*[0]
+
+    return RESULT
